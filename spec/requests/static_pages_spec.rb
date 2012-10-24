80ec65e7833a4c3e8bc1ddebacc9ba47c81bfc96
Andrew Reeves <reevesand@gmail.com>
2012-10-24 13:06:12 -0500

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,61 +2,52 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-	let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+  subject { page }
 
   describe "Home page" do
+    before { visit root_path}
 
-    it "should have the h1 'Sample App'" do
-      visit root_path
-      page.should have_selector('h1', :text => 'Sample App')
-    end
+    it { should have_selector('h1', text: 'Sample App')}
+    it { should have_selector('title', text: full_title(""))}
 
-    it "should have the base title" do
-      visit root_path
-      page.should have_selector('title',
-                        :text => "#{base_title}")
-    end
   end
 
   describe "Help page" do
+    before { visit help_path}
 
     it "should have the h1 'Help'" do
-      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the title 'Help'" do
-      visit help_path
       page.should have_selector('title',
-                        :text => "#{base_title} | Help")
+                        :text => full_title("Help"))
     end
   end
 
   describe "About page" do
+    before { visit about_path}
 
     it "should have the h1 'About Us'" do
-      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit about_path
       page.should have_selector('title',
-                    :text => "#{base_title} | About Us")
+                    :text => full_title("About Us"))
     end
   end
 
-    describe "Contact page" do
+  describe "Contact page" do
+    before { visit contact_path}
 
     it "should have the h1 'Contact'" do
-      visit contact_path
       page.should have_selector('h1', :text => 'Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit contact_path
       page.should have_selector('title',
-                    :text => "#{base_title} | Contact")
+                    :text => full_title('Contact'))
     end
   end
 end
